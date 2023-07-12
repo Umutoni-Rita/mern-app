f4254ffea817cc5db3cc9e39f7ee17135a4b4b1a
Umutoni-Rita <umurita37@gmail.com>
2023-07-12 10:33:57 +0200

    Registration feature

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,19 +1,26 @@
-import express  from "express";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { userModel } from "../models/users.js";
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
-router.post("/register", async (req, res) => {
-    const {username, password} = req.body;
 
-    const user = await userModel.findOne({username});
-    res.json(user);
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (user) {
+    return res.json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, hashedPassword });
+  await newUser.save();
+
+  res.json({message: 'User created successfully'});
 });
 
+router.post("/login");
 
-
-
-
-
-export {router as userRouter};
+export { router as userRouter };
